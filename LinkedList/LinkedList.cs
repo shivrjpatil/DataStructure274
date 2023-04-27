773fefc7d87808d81c03c29d3f11f08571b4e4c8
shivrjpatil <shivrajp972@gmail.com>
2023-04-27 15:28:22 +0530

    Added custom linked list program

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        Node head;
+        public void AddNode(int data)
+        {
+            Node node = new Node(data);
+            if(head==null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+
+                while(temp.Next!=null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} inserted into linkedlist",node.data);
+        }
+
+        public void AddReverseData(int data)
+        {
+            Node newNode=new Node(data);
+            if(this.head==null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.Next = temp;
+            }
+
+        }
+
+        public void PopLast()
+        {
+            Node tempnode = head;
+            Node lastnode=tempnode.Next;
+            if(head.Next!=null)
+            {
+                tempnode = tempnode.Next;
+                tempnode.Next = null;
+                lastnode = null;
+            }
+        }
+        public void dispaly()
+        {
+            if(head==null)
+            {
+                Console.WriteLine("Linked list is empty");
+            }
+            else
+            {
+                Node tempnode = head;
+
+                while(tempnode!=null)
+                {
+                    Console.WriteLine(tempnode.data);
+                    tempnode = tempnode.Next;
+                }
+            }
+        }
+    }
+}
