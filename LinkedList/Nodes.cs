cf8fca1e3271d41f422e543897d468f0eb9cdba8
shivrjpatil <shivrajp972@gmail.com>
2023-04-27 15:37:55 +0530

    Added custom Stack Program

---
+++ LinkedList/Nodes.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Nodes
+    {
+        public int item;
+        public Nodes left;
+        public Nodes right;
+
+        public Nodes(int data)
+        {
+            item = data;
+            left = null;
+            right = null;
+
+        }
+    }
+    internal class BinarySearch
+    {
+        public Nodes root;
+        public BinarySearch()
+        {
+            root = null;
+        }
+
+        public Nodes ReturnRoot()
+        {
+            return root;
+        }
+
+        public void Insert(int id)
+        {
+            Nodes newNode =new Nodes(id);
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Nodes current = root;
+                Nodes parent;
+                while(true)
+                {
+                    parent = current;
+                    if(id<current.item)
+                    {
+                        current = current.left;
+                        if(current==null)
+                        {
+                            parent.left = newNode;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if(current==null)
+                        {
+                            parent.right = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        public void Display(Nodes Root)
+        {
+           if(Root != null)
+            {
+                Console.WriteLine(Root.item + " ");
+                Display(Root.left);
+                Display(Root.right);
+            }
+        }
+
+        public void Post(Nodes Root)
+        {
+            if(Root != null)
+            {
+                Post(Root.left);
+                Post(Root.right);
+                Console.WriteLine(Root.item + " ");
+            }
+        }
+        public void Inorder(Nodes Root)
+        {
+            if (Root != null)
+            {
+               Inorder(Root.left);
+                Console.WriteLine(Root.item + " ");
+                
+                Inorder(Root.right);
+            }
+        }
+
+    }
+}
---
+++ LinkedList/Stack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Stack
+    {
+        public Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.Next = null;
+            }
+            else
+            {
+                node.Next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack", this.top.data);
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            Console.WriteLine("Value Popped is {0}", this.top.data);
+            this.top = this.top.Next;
+        }
+       
+        public void Display()
+        {
+            Node temp = this.top;
+            Console.WriteLine("Stack is as:");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.Next;
+            }
+        }
+    }
+}
